51c7400137621446b986e4baf55b5b9047ce6245
josegonz7 <jgoncre1412@g.educaand.es>
2026-03-09 10:22:04 +0100

    sa

---
+++ UD05/src/ejerciciocola/Cola.java
@@ -0,0 +1,8 @@
+package ejerciciocola;
+
+public interface Cola {
+
+    void encolar(int n);
+
+    int desencolar();
+}
---
+++ UD05/src/ejerciciocola/Pila.java
@@ -0,0 +1,8 @@
+package ejerciciocola;
+
+public interface Pila {
+
+	void apilar(int n);
+
+	int desapilar();
+}
